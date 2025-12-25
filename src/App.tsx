f4b74580f48a8a6de41b799fc2ece72e881d19f6
DanriWeb <danri.web@gmail.com>
2025-12-25 16:40:00 +0500

    feat(ui): :lipstick: доработка тегов в App и Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import "./App.css";
 import Header from "./components/Header";
+import CategoryList from "./components/CategoryList";
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <main className="app__main">
+        <div className="app__main-title-container">
+          <h2 className="app__main-title">Выберите категорию проекта</h2>
+        </div>
+        <CategoryList />
+      </main>
     </div>
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,12 @@ export default function Header() {
   return (
     <header className="header">
       <div className="header__title-container">
-        <h1 className="header__title">MVP Freelance Calculator</h1>
+        <h1 className="header__title">
+          <span className="header__title--full">
+            💎 MVP Freelance Calculator
+          </span>
+          <span className="header__title--short">💎 MVP FC</span>
+        </h1>
       </div>
     </header>
   );
