74ab1dba3692975489446faa401965e3eb14af43
DanriWeb <danri.web@gmail.com>
2025-12-26 23:18:46 +0500

    docs(docs): :memo: обновить README и визуальные элементы проекта

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,10 +5,8 @@ export default function Header() {
     <header className="header">
       <div className="header__title-container">
         <h1 className="header__title">
-          <span className="header__title--full">
-            💎 MVP Freelance Calculator
-          </span>
-          <span className="header__title--short">💎 MVP FC</span>
+          <span className="header__title--full">💎 Freelance Calculator</span>
+          <span className="header__title--short">💎 FC</span>
         </h1>
       </div>
     </header>
